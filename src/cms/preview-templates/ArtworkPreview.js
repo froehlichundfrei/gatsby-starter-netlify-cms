4335a3f395ae2de3f4c46537475a7f19910d6b09
Janine Dubslaff <janine@froehlichundfrei.de>
2020-05-27 16:04:04 +0200

    feat: modify yaml for artwork folder collection

--- src/cms/preview-templates/ArtworkPreview.js
+++ src/cms/preview-templates/ArtworkPreview.js
@@ -5,15 +5,12 @@ import { BlogPostTemplate } from "../../templates/blog-post";
 const ArtworkPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(["data", "tags"]);
   return (
-    <>
-      moep
-      <BlogPostTemplate
-        content={widgetFor("body")}
-        description={entry.getIn(["data", "description"])}
-        tags={tags && tags.toJS()}
-        title={entry.getIn(["data", "title"])}
-      />
-    </>
+    <BlogPostTemplate
+      content={widgetFor("body")}
+      description={entry.getIn(["data", "description"])}
+      tags={tags && tags.toJS()}
+      title={entry.getIn(["data", "title"])}
+    />
   );
 };
 
